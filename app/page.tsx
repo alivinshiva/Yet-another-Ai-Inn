dad6ce650173c7c751e237a5dbe4ac1f116c68d2
Alivin Shiva <alivin612003@gmail.com>
2024-06-26 15:12:25 +0530

    removed unwanted code

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,21 @@ import logo from '@/Images/applogo.png'
 import { Button } from "@/components/ui/button";
 import Link from 'next/link'
 import Writer from "@/components/Writer";
-// import Stories from "@/components/Stories";
+import Header from "@/components/Header";
+
 
 
 export default function Home() {
   return (
     <main className="flex-1 flex flex-col">
+            {/* <Header /> */}
+
       <section className="flex-1 grid grid-cols-1 lg:grid-cols-2">
         {/* Ui element designe purpose only */}
         <div className="bg-purple-500 flex flex-col space-y-5 justify-center items-center order-1 lg:-order-1 pb-10">
           <Image src={logo} height={250} alt="App logo" />
           <Button asChild className="px-20 bg-purple-700 p-10 text-xl">
-            <Link href='/Stories'> Explore Story Library </Link>
+            <Link href='/stories'> Explore Story Library </Link>
           </Button>
         </div>
 
---
+++ lib/cleanTitle.ts
@@ -0,0 +1,7 @@
+function cleanTitle(title: string): string {
+    return title
+        .replace(/[-/\\[\]]/g, " ")
+        .replace(/\s+/g, " ")
+        .trim();
+}
+export default cleanTitle
